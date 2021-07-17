5498f0c45a5045e66f18a7bb28f0cf01227e2fd4
GandresCoello18 <goyeselcoca@gmail.com>
2021-07-17 18:58:03 -0500

    mover carpeta de test dentro de src

--- src/services/user/__tests__/user.unit.test.ts
+++
@@ -1,9 +0,0 @@
-import { getUserUtil } from '../../../utils';
-
-describe('TEST STORAGE USER', () => {
-  test('obtener usuario', async () => {
-    const users = await getUserUtil({ email: 'goyeselcoca@gmail.com' });
-
-    expect(users).toHaveLength(1);
-  });
-});
